b267b40aa96562ce2162c1910b4beb3b03ef10b9
shihab <echoshihab@gmail.com>
2019-11-23 19:56:25 -0500

    add edit mode to display activity edit form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,12 +9,16 @@ interface Iprops {
   activities: IActivity[];
   selectActivity: (id: string) => void;
   selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
 }
 
 const ActivityDashboard: React.FC<Iprops> = ({
   activities,
   selectActivity,
-  selectedActivity
+  selectedActivity,
+  editMode,
+  setEditMode
 }) => {
   return (
     <Grid>
@@ -22,8 +26,13 @@ const ActivityDashboard: React.FC<Iprops> = ({
         <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && <ActivityDetails activity={selectedActivity} />}
-        <ActivityForm />
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+          />
+        )}
+        {editMode && <ActivityForm />}
       </Grid.Column>
     </Grid>
   );
