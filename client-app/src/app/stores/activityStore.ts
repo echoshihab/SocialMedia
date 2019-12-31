f3a417ba79b0eb653c25bdca110716153f81530f
shihab <echoshihab@gmail.com>
2019-12-30 19:38:42 -0500

    use key to reset edit-create component state

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -9,7 +9,7 @@ configure({
 class ActivityStore {
   @observable activityRegistry = new Map();
   @observable activities: IActivity[] = [];
-  @observable activity: IActivity | undefined;
+  @observable activity: IActivity | null = null;
   @observable loadingInitial = false;
   @observable editMode = false;
   @observable submitting = false;
@@ -59,6 +59,10 @@ class ActivityStore {
         console.log(error);
       }
     }
+  };
+
+  @action clearActivity = () => {
+    this.activity = null;
   };
 
   getActivity = (id: string) => {
@@ -127,7 +131,7 @@ class ActivityStore {
 
   @action openCreateForm = () => {
     this.editMode = true;
-    this.activity = undefined;
+    this.activity = null;
   };
 
   @action openEditForm = (id: string) => {
@@ -136,7 +140,7 @@ class ActivityStore {
   };
 
   @action cancelSelectedActivity = () => {
-    this.activity = undefined;
+    this.activity = null;
   };
 
   @action cancelFormOpen = () => {
