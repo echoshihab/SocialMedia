85b09745dc26af350a5a6936b9824c76b1d33248
shihab <echoshihab@gmail.com>
2020-01-17 19:32:58 -0500

    create and apply reusable date component

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -21,11 +21,11 @@ class ActivityStore {
 
   groupActivitiesByDate(activities: IActivity[]) {
     const sortedActivities = activities.sort(
-      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+      (a, b) => a.date!.getTime() - b.date!.getTime()
     );
     return Object.entries(
       sortedActivities.reduce((activities, activity) => {
-        const date = activity.date.split("T")[0];
+        const date = activity.date!.toISOString().split("T")[0];
         activities[date] = activities[date]
           ? [...activities[date], activity]
           : [activity];
@@ -40,7 +40,7 @@ class ActivityStore {
       const activities = await agent.Activities.list();
       runInAction("loading activities", () => {
         activities.forEach(activity => {
-          activity.date = activity.date.split(".")[0];
+          activity.date = new Date(activity.date!);
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
@@ -67,6 +67,7 @@ class ActivityStore {
         });
       } catch (error) {
         runInAction("get activity error", () => {
+          activity.date = new Date(activity.date);
           this.loadingInitial = false;
         });
         console.log(error);
